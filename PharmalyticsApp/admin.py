0df46455b83d380efd5ab1b06219879b076fbb2c
Sara Abbas <sia36@mail.aub.edu>
2023-11-28 21:09:57 +0200

    'Customer' templates creation

--- PharmalyticsApp/admin.py
+++ PharmalyticsApp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Medication
+from .models import Medication, Customers
 
 # Register your models here.
 
 admin.site.register(Medication)
+admin.site.register(Customers)
